4a4f2e162e4c3e8f8d87bb89950c090944509c98
kkb <gimgyubin1225@gmail.com>
2025-12-26 17:59:51 +0900

    chore: initial setup

---
+++ src/test/java/com/omteam/omt/OmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omteam.omt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
