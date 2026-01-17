2c26f540d6c4c19f05c6a8c4555f8fd0d65a53bb
kkb <gimgyubin1225@gmail.com>
2026-01-17 11:12:13 +0900

    feat: 소셜 로그인 사용자 계정 매핑 저장 로직 추가

--- src/main/java/com/omteam/omt/user/repository/UserSocialAccountRepository.java
+++ src/main/java/com/omteam/omt/user/repository/UserSocialAccountRepository.java
@@ -1,9 +1,13 @@
 package com.omteam.omt.user.repository;
 
+import com.omteam.omt.user.domain.SocialProvider;
 import com.omteam.omt.user.domain.UserSocialAccount;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserSocialAccountRepository extends JpaRepository<UserSocialAccount, Long> {
-    <T> Optional<T> findByProviderUserId(String providerUserId);
+    Optional<UserSocialAccount> findByProviderAndProviderUserId(
+            SocialProvider provider,
+            String providerUserId
+    );
 }
