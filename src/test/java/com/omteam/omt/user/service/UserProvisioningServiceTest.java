b6c419d7a9f74101867f99eb79a113d94543a6d9
kkb <gimgyubin1225@gmail.com>
2026-02-03 23:17:37 +0900

    fix: 회원 탈퇴 후 재가입 시 Unique Constraint 위반 버그 수정
    탈퇴한 사용자가 동일한 소셜 계정으로 재가입할 때 발생하는
    Duplicate entry 오류를 수정
    
    - UserSocialAccount에 updateUser() 메서드 추가
    - 재가입 시 새 SocialAccount 생성 대신 기존 SocialAccount를
      새 User에 연결하도록 로직 변경

--- src/test/java/com/omteam/omt/user/service/UserProvisioningServiceTest.java
+++ src/test/java/com/omteam/omt/user/service/UserProvisioningServiceTest.java
@@ -127,18 +127,21 @@ class UserProvisioningServiceTest {
     }
 
     @Test
-    @DisplayName("재가입 - 탈퇴한 사용자의 소셜 계정으로 로그인 시 새 계정 생성")
+    @DisplayName("재가입 - 탈퇴한 사용자의 소셜 계정으로 로그인 시 새 User 생성 후 기존 SocialAccount 연결")
     void findOrCreateUser_creates_new_user_when_existing_is_withdrawn() {
         // given
         User withdrawnUser = spy(createUser());
         given(withdrawnUser.isActive()).willReturn(false);
-        UserSocialAccount socialAccount = UserSocialAccount.builder()
+        UserSocialAccount socialAccount = spy(UserSocialAccount.builder()
                 .user(withdrawnUser)
                 .provider(provider)
                 .providerUserId(providerUserId)
-                .build();
+                .build());
 
-        User newUser = createUser();
+        User newUser = User.builder()
+                .userId(2L)
+                .email(email)
+                .build();
         given(socialAccountRepository.findByProviderAndProviderUserId(provider, providerUserId))
                 .willReturn(Optional.of(socialAccount));
         given(userRepository.save(any(User.class))).willReturn(newUser);
@@ -149,7 +152,8 @@ class UserProvisioningServiceTest {
         // then
         assertThat(result).isEqualTo(newUser);
         then(userRepository).should().save(any(User.class));
-        then(socialAccountRepository).should().save(any(UserSocialAccount.class));
+        then(socialAccount).should().updateUser(newUser);
+        then(socialAccountRepository).should(never()).save(any(UserSocialAccount.class));
         then(characterRepository).should().save(any(UserCharacter.class));
     }
 
